82447e017d9dc1f92f99119a03a3e6682b217f49
Luis Gomes <luisgomesbcc@gmail.com>
2020-05-31 20:50:27 -0300

    "versão 02"

--- resources/views/Areas/show.blade.php
+++ resources/views/Areas/show.blade.php
@@ -1,31 +1,35 @@
 @extends('layout.app')
 @section('title',$area->titulo)
 @section('content')
-        <h1>areas - {{$area->titulo}}</h1>
+        <h1>Indicadores - {{$area->titulo}}</h1>
+        
         <div class="row">
+        
             @if(file_exists("./img/areas/".md5($area->id).".jpg"))
             <div class="col-md-6">
                 <img src="{{url('img/areas/'.md5($area->id).'.jpg')}}" alt="Imagem area" class="img-fluid img-thumbnail">
             </div>
             @endif
             <div class="col-md-6">
-            <ul>
+            <!-- <ul>
                 <li><strong>SKU: </strong> {{$area->sku}} </li>
                 <li><strong>Local: </strong> {{$area->local}} </li>
                 <li><strong>Adicionado em: </strong> {{date("d/m/Y H:i",strtotime($area->created_at))}} </li>
-            </ul>
+            </ul> -->
         
             <container name="cartaoIndicadores">
+            <span class="wob_t" id="wob_tm" style="display:inline">26</span>
             @foreach($indicadores as $indicador)
             <div class="card border-success mb-3" style="max-width: 18rem;">
-            <div class="card-header bg-transparent border-success">Header</div>
+            <div class="card-header bg-transparent border-success">{{$indicador->titulo}}</div>
             <div class="card-body text-success">
-                <h5 class="card-title">{{$indicador->titulo}}</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title">24º</h5>
+                <!-- <p class="card-text">C</p> -->
             </div>
-            <div class="card-footer bg-transparent border-success">Footer</div>
+            <!-- <div class="card-footer bg-transparent border-success">Footer</div> -->
             </div>
             @endforeach
+            
             </container>
 
 
@@ -42,6 +46,8 @@
             
             
         </div>
-        
         <a class="btn btn-primary" href="javascript:history.go(-1)">Voltar</a>
+        
+        
+        
 @endsection
--- resources/views/Layout/app.blade.php
+++ resources/views/Layout/app.blade.php
@@ -1,81 +1,64 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
-    <head>
 
-        <title>Tupã - @yield('title')</title>
-        <link rel="stylesheet" type="text/css" href="{{URL::to('dist/css/bootstrap.min.css')}}">
-    </head>
-    <body>
+<head>
 
-    <!--  -->
-    <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('areas/')}}" >Home</a>
+  <title>Tupã - @yield('title')</title>
+  <link rel="stylesheet" type="text/css" href="{{URL::to('dist/css/bootstrap.min.css')}}">
+</head>
+
+<body>
+
+<div style="center-text" class="alert alert-warning alert-dismissible fade show" role="alert">
+      <strong>Não se preocupe!</strong> Atualizações futuras irão mudar o estilo e o funcionamento da plataforma.
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+
+
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+   
+    <a class="navbar-brand">Tupã</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="{{url('areas')}}">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('areas/create')}}">Create</a>
-        </li> -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Time
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" target="_blank" href="https://twitter.com/vilailus">Luis Antônio da Curz Gomes</a>
+            <a class="dropdown-item" target="_blank" href="#">Abima</a>
+            <a class="dropdown-item" target="_blank" href="#">Ludy</a>
+            <a class="dropdown-item" target="_blank" href="#">Darlison</a>
+            <a class="dropdown-item" target="_blank" href="#">Vanessa</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" target="_blank" href="http://www.ufopa.edu.br/ufopa/">UFOPA</a>
+          </div>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        @yield('searchNav')
+      </form>
+    </div>
+  </nav>
 
-        
-        
-        
-
-
-        <!--  -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="http://www.ufopa.edu.br/ufopa/">UFOPA</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="{{url('areas/')}}">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="{{url('areas/create')}}">Create</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Team Members
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Abima</a>
-          <a class="dropdown-item" href="#">Darlison</a>
-          <a class="dropdown-item" href="#">Ludy</a>
-          <a class="dropdown-item" href="#">Luis G</a>
-          <a class="dropdown-item" href="#">Sany</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Time Tupã</a>
-        </div>
-      </li>
-    </ul>
-    @yield('searchNav')
+  <div class="container">
+    @yield('content')
   </div>
-</nav>
+  <script src="js/bootstrap.bundle.min.js"></script> <!-- Menu Toggle Script -->
+  <script type="text/javascript" src="{{URL::to('/dist/js/jquery.min.js')}}"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
 
-        <!--  -->
-
-
-    </nav>
-    
-
-    <!-- ---------------------------------------------------------------------- -->
-
-    
-
-
-
-    <!--  -->
-    
-
-    
-
-        <div class="container">
-            @yield('content')
-        </div>
-        <script src="js/bootstrap.bundle.min.js"></script> <!-- Menu Toggle Script -->
-        <script type="text/javascript" src="{{URL::to('/dist/js/jquery.min.js')}}"></script>
-    </body>
 </html>
